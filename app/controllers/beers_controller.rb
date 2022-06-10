269e76e26736cabf29f0f643aae021ea1a45d47a
faraznaeem <faraznaeem@gmail.com>
2022-06-10 15:46:16 +0200

    Refactors controller

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,36 +1,34 @@
 class BeersController < ApplicationController
   include Pagy::Backend
 
+  URI = 'https://api.punkapi.com/v2/beers'
+
   def index
-    response = JSON.parse(RestClient.get('https://api.punkapi.com/v2/beers?per_page=80').body).first(6)
-    @beers = response
-    # @random_beer = RestClient.get('https://api.punkapi.com/v2/beers/random')
+    response = RestClient.get(URI)
+    parsed_response = JSON.parse(response.body).first(6)
+    @beers = parsed_response
   end
 
   def show
-    response = RestClient.get("https://api.punkapi.com/v2/beers/#{params[:id]}")
+    response = RestClient.get(URI+"/#{params[:id]}")
     @beer = JSON.parse(response.body).first
   end
 
   def search_beer
     @search_beer = params[:query]
-    response = JSON.parse(RestClient.get("https://api.punkapi.com/v2/beers?beer_name=#{@search_beer}&per_page=80"))
-    # binding.pry
-    # @search_result = response
-    @pagy, @search_result = pagy_array(response, items: 10)
 
-    # Todo: Refactor this to use a loop to get all the pages
-    # __________________________________
-    # @search_beer_array = []
-    # if response.count < 80
-    # elsif response.count == 80
-    #   page = JSON.parse(RestClient.get("https://api.punkapi.com/v2/beers?beer_name=#{search_beer}&page=#{2}&per_page=80"))
-    #   @search_beer_array << response
-    #   @search_beer_array << page
-    # else
-    #   return @search_result = "No results found"
-    # end
-    # __________________________________
+    if @search_beer.ascii_only?
+      response = JSON.parse(RestClient.get(URI+"?beer_name=#{@search_beer}&per_page=80").body)
+      message = "No beers found for #{@search_beer}"
 
+      if response.empty?
+        redirect_to root_path, notice: message
+      else
+        @pagy, @search_result = pagy_array(response, items: 10)
+      end
+
+    else
+      redirect_to root_path, notice: message
+    end
   end
 end
